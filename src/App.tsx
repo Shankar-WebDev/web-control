bfae315d8a4d52b6a40e891edab83a2bb18b6ead
RishikesavanRamesh <rishikesavanramesh@proton.me>
2024-09-13 01:16:33 +0530

    Update minimize

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,48 @@ import Section from './components/Section';
 function App() {
   const [activeSection, setActiveSection] = useState('');
 
+  const [section1State, setSection1State] = useState('idle');
   const [section2State, setSection2State] = useState('idle');
+  const [section3State, setSection3State] = useState('idle');
+  const isSection1Maximized = () => section1State === 'maximized';
   const isSection2Maximized = () => section2State === 'maximized';
-  const isSection2Minimized = () => section2State === 'minimized';
+  const isSection3Maximized = () => section3State === 'maximized';
 
   const handleSectionClick = (section: string) => {
     setActiveSection(section);
   };
 
   const handleState = (section: string, state: string) => {
-    if (state === 'maximize') {setSection2State('maximized')}
-    if (state === 'minimize') {setSection2State('minimized')}
+    if (section === 'section2') {
+      if (state === 'maximize') {
+        if (section3State === 'maximized') { setSection3State('minimized') }
+        setSection2State('maximized');
+      }
+      if (state === 'minimize') {
+        setSection2State('minimized');
+      }
+    } else if (section === 'section3') {
+      if (state === 'maximize') {
+        if (section2State === 'maximized') { setSection2State('minimized') }
+        setSection3State('maximized');
+      }
+      if (state === 'minimize') {
+        setSection3State('minimized');
+      }
+    } else if (section === 'section1') {
+      if (state === 'maximize') {
+        setSection1State('maximized');
+      }
+      if (state === 'minimize') {
+        setSection1State('minimized');
+      }
+    }
     setActiveSection(section);
   };
 
   return (
     <div className="text-white h-screen flex flex-col bg-[#0f0f0f]">
-      <div className="h-[50px] w-full flex justify-center items-center justify-between p-[10px]">
+      <div className="h-[50px] w-auto flex justify-center items-center justify-between p-[10px]">
         <RosIcon width={40} height={40} color="#a1a1a1" />
         <div className='flex items-center bg-[#333333] h-[30px] p-2 rounded-[5px] text-center'>
           <span className='underline text-gray-400 hover:text-green-500'>http://localhost:8000</span>
@@ -31,35 +56,44 @@ function App() {
       <div className="flex-1 flex flex-row p-[10px] pt-[0px] gap-[10px]">
 
 
-        <Section section='section1' activeSection={activeSection} handleSectionClick={() => handleSectionClick('section1')}> <h1>          <a className="relative text-start p-4 pt-0 pb-0 rounded-[5px] flex group/heading items-center">
-          <div className="border-r-2 border-[#464646] absolute right-0 h-3"></div>
-          <div className="absolute inset-0 -right-[2px] -top-[2px] -bottom-[2px] bg-[#464646] opacity-0 group-hover/heading:opacity-100 rounded-[5px]"></div>
-          <span className="relative z-10">Topics</span>
-        </a>
-          <a className="relative text-start p-4 pt-0 pb-0 rounded-[5px] flex group/heading items-center">
-            <div className="absolute inset-0 -left-[2px] -right-[2px] -top-[2px] -bottom-[2px] bg-[#464646] opacity-0 group-hover/heading:opacity-100 rounded-[5px]"></div>
-            <span className="relative z-10">Services</span>
-          </a>
-          <a className="relative text-start p-4 pt-0 pb-0 rounded-[5px] flex group/heading items-center">
-            <div className="border-r-2 border-[#464646] absolute left-0 h-3"></div>
-            <div className="absolute inset-0 -left-[2px] -right-[2px] -top-[2px] -bottom-[2px] bg-[#464646] opacity-0 group-hover/heading:opacity-100 rounded-[5px]"></div>
-            <span className="relative z-10">Actions</span>
-          </a>
 
-        </h1>
+
+        <Section sideMinimized={true} section='section1'
+          minimized={isSection1Maximized()}
+          activeSection={activeSection} handleState={(state) => handleState('section1', state)} handleSectionClick={(section) => handleSectionClick(section)} >
+          <h1>          <a className="relative text-start p-4 pt-0 pb-0 rounded-[5px] flex group/heading items-center">
+            <div className="border-r-2 border-[#464646] absolute right-0 h-3"></div>
+            <div className="absolute inset-0 -right-[2px] -top-[2px] -bottom-[2px] bg-[#464646] opacity-0 group-hover/heading:opacity-100 rounded-[5px]"></div>
+            <span className="relative z-10">Topics</span>
+          </a>
+            <a className="relative text-start p-4 pt-0 pb-0 rounded-[5px] flex group/heading items-center">
+              <div className="absolute inset-0 -left-[2px] -right-[2px] -top-[2px] -bottom-[2px] bg-[#464646] opacity-0 group-hover/heading:opacity-100 rounded-[5px]"></div>
+              <span className="relative z-10">Services</span>
+            </a>
+            <a className="relative text-start p-4 pt-0 pb-0 rounded-[5px] flex group/heading items-center">
+              <div className="border-r-2 border-[#464646] absolute left-0 h-3"></div>
+              <div className="absolute inset-0 -left-[2px] -right-[2px] -top-[2px] -bottom-[2px] bg-[#464646] opacity-0 group-hover/heading:opacity-100 rounded-[5px]"></div>
+              <span className="relative z-10">Actions</span>
+            </a>
+
+          </h1>
 
           <h2>        <ArrowLeftFromLine className='p-[5px]' />
 
             <span className="relative">Back</span></h2>
 
           <body>content</body></Section>
-        <div className='flex flex-col min-w-[50vw] gap-[10px]'>
+
+
+        <div className='flex-1 flex flex-col gap-[10px]'>
 
 
           <Section section='section2'
             minimized={isSection2Maximized()}
             activeSection={activeSection} handleState={(state) => handleState('section2', state)} handleSectionClick={(section) => handleSectionClick(section)} ><h1> Macros</h1> section1</Section>
-          <Section section='section3' activeSection={activeSection} handleSectionClick={(section) => handleSectionClick(section)}> <h1>Log</h1> section1</Section>
+          <Section section='section3'
+            minimized={isSection3Maximized()}
+            activeSection={activeSection} handleState={(state) => handleState('section3', state)} handleSectionClick={(section) => handleSectionClick(section)} ><h1> Macros</h1> section2</Section>
         </div>
       </div>
     </div>
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode, Children, ReactElement } from 'react';
 import { Maximize, Minimize, ChevronDown, ChevronUp, ChevronRight, ChevronLeft, ArrowLeftFromLine } from 'lucide-react'; // Ensure the path is correct for your icons
 
 interface SectionProps {
+  sideMinimized?: Boolean;
   minimized: Boolean;
   fullscreen: Boolean;
   section: string;
@@ -11,7 +12,7 @@ interface SectionProps {
   children?: ReactNode;
 }
 
-const Section: React.FC<SectionProps> = ({ section, minimized, fullscreen, activeSection, handleSectionClick, handleState, children }) => {
+const Section: React.FC<SectionProps> = ({sideMinimized, section, minimized, fullscreen, activeSection, handleSectionClick, handleState, children }) => {
   // Helper functions to extract specific elements
   const extractElement = (type: React.ElementType) => {
     const found = Children.toArray(children).find(child => React.isValidElement(child) && (child as ReactElement).type === type);
@@ -21,16 +22,16 @@ const Section: React.FC<SectionProps> = ({ section, minimized, fullscreen, activ
   const headingContent = extractElement('h1');
   const subHeadingContent = extractElement('h2');
   const bodyContent = React.Children.toArray(children).filter(child => !React.isValidElement(child) || (child as ReactElement).type !== 'h1' && (child as ReactElement).type !== 'h2');
-  console.log('minimized', minimized)
+  
   return (
     <section
-      className={`${minimized ? 'h-[40px] w-[300px] bg-red-300' : 'bg-red-600'} relative flex-1 bg-[#262626] text-[#a1a1a1] rounded-[10px] box-border group ${activeSection === section ? 'border-green-500' : ''}`}
+      className={`${minimized ? 'h-[40px]' : 'h-full'} ${(minimized && sideMinimized)? 'absolute left-[40px] transform origin-top-left rotate-90' : ""} w-[50vw] relative flex-basis bg-[#262626] text-[#a1a1a1] rounded-[10px] box-border group ${activeSection === section ? 'border-green-500' : ''}`}
       onClick={() => handleSectionClick(section)} // Use the section prop for the click handler
     >
       {/* Outset Border Effect */}
       <div className={`absolute inset-0 border-[1px] ${activeSection === section ? 'border-[#5d5d5d]' : 'border-transparent'} rounded-[10px] border-outset pointer-events-none`} />
 
-      <h1 className={`h-[40px] bg-[#333333] rounded-t-[10px] inset-0 border-[1px] border-outset ${activeSection === section ? 'border-[#5d5d5d]' : 'border-transparent'} flex items-center justify-between px-[5px] relative`}>
+        <h1 className={`h-[40px] bg-[#333333] rounded-t-[10px] ${minimized ? 'rounded-b-[10px]' : ''} inset-0 border-[1px] border-outset ${activeSection === section ? 'border-[#5d5d5d]' : 'border-transparent'} flex items-center justify-between px-[5px] relative`}>
         <div className="flex items-center">
           {headingContent}
         </div>
@@ -40,15 +41,15 @@ const Section: React.FC<SectionProps> = ({ section, minimized, fullscreen, activ
           <Minimize className="hover:bg-[#464646] p-[5px] text-[#a1a1a1] h-[30px] w-[30px] rounded-[5px]" />
           {minimized && <ChevronUp className="hover:bg-[#464646] p-[5px] text-[#a1a1a1] h-[30px] w-[30px] rounded-[5px]"  onClick={() => {console.log('down clicked');handleState('minimize')}}/>}
           {!minimized &&<ChevronDown className="hover:bg-[#464646] p-[5px] text-[#a1a1a1] h-[30px] w-[30px] rounded-[5px]" onClick={() => {console.log('up clicked');handleState('maximize')}} />}
-          <ChevronRight className="hover:bg-[#464646] p-[5px] text-[#a1a1a1] h-[30px] w-[30px] rounded-[5px]" />
-          <ChevronLeft className="hover:bg-[#464646] p-[5px] text-[#a1a1a1] h-[30px] w-[30px] rounded-[5px]" />
+          {/* <ChevronRight className="hover:bg-[#464646] p-[5px] text-[#a1a1a1] h-[30px] w-[30px] rounded-[5px]" />
+          <ChevronLeft className="hover:bg-[#464646] p-[5px] text-[#a1a1a1] h-[30px] w-[30px] rounded-[5px]" /> */}
         </div>
       </h1>
       <h2 className={`${minimized ? 'hidden' : ''} h-[35px] bg-[#262626] border-b-2 border-[#333333] flex justify-start gap-[5px] px-[5px] items-center`}>
 
         {subHeadingContent}
       </h2>
-      <div className='p-5'>
+      <div className={`p-5 ${minimized ? 'hidden' : ''}`}>
         {bodyContent} {/* Render remaining children directly */}
       </div>
     </section>
